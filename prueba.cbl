1185e4918a0464e99415c30bccbbb289ea83d4cf
Hernán Cortés <hacortesus@gmail.com>
2023-06-13 15:45:54 -0400

    primera subida

---
+++ prueba.cbl
@@ -0,0 +1,8 @@
+       identification division.
+       program-id. prueba.
+       data division.
+       working-storage section.
+       procedure division.
+       partir.
+           display "buenas noches".
+           stop run.
