092ea93c9bb29fa08d643d6d559000e155e73909
Hernán Cortés <hacortesus@gmail.com>
2023-06-13 15:48:20 -0400

    subida del resto

---
+++ test2.cbl
@@ -0,0 +1,8 @@
+       identification division.
+       program-id. test2.
+       data division.
+       working-storage section.
+       procedure division.
+       partir.
+           display "modificacion1 de test2.cbl".
+           stop run.
